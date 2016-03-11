6e69319502d5eefc10d0baf7b5ae1ca92a869886
Sandro Santilli <strk@keybit.net>
2016-03-11 17:40:56 +0100

    Do not hard-code webmercator SRS
    Avoids forced reprojection with consequent visible tile boundaries.
    Fixes #35 and #31

--- src/pile.js
+++ src/pile.js
@@ -1116,7 +1116,10 @@ module.exports = pile = {
 
 		var options = {
 			// srid 3857
-			"srs": "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0.0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs +over",
+			// NOTE: map srs should be already set at this point,
+			//			 and hard-coding it is not a good idea, see
+			//			 https://github.com/systemapic/pile/issues/35
+      //"srs": mercator.proj4,
 
 			"Stylesheet": [{
 				"id" : 'tile_style',
