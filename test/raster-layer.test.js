0c44e9de0648eebf04722d52f30f818e4daa0594
knutole <knutolesjoli@gmail.com>
2016-04-14 14:32:28 +0200

    cleanup

--- test/raster-layer.test.js
+++ test/raster-layer.test.js
@@ -229,177 +229,177 @@ describe('Raster', function () {
         }); 
 
 
-        it('should vectorize', function (done) {
-            this.timeout(121000);
-            token(function (err, access_token) {
-                api.post(endpoints.data.vectorize)
-                .send({file_id : tmp.upload_status.file_id, access_token : access_token})
-                .expect(httpStatus.OK)
-                .end(function (err, res) {
-                    if (err) return done(err);
+        // it('should vectorize', function (done) {
+        //     this.timeout(121000);
+        //     token(function (err, access_token) {
+        //         api.post(endpoints.data.vectorize)
+        //         .send({file_id : tmp.upload_status.file_id, access_token : access_token})
+        //         .expect(httpStatus.OK)
+        //         .end(function (err, res) {
+        //             if (err) return done(err);
 
-                    var status = res.body;
+        //             var status = res.body;
                     
-                    tmp.vectorized_status = status;
+        //             tmp.vectorized_status = status;
                     
-                    if (debugMode) {
-                        console.log('\n\n\n');
-                        console.log('Dataset returned from', endpoints.data.vectorize, '[pile]:');
-                        console.log('------------------------------------------')
-                        console.log(status);
-                    }
+        //             if (debugMode) {
+        //                 console.log('\n\n\n');
+        //                 console.log('Dataset returned from', endpoints.data.vectorize, '[pile]:');
+        //                 console.log('------------------------------------------')
+        //                 console.log(status);
+        //             }
 
-                    expect(status.user_id).to.exist;
-                    expect(status.file_id).to.exist;
-                    expect(status.filename).to.be.equal(tmp.upload_status.filename);
-                    expect(status.status).to.be.equal('Processing');
-                    expect(status.data_type).to.be.equal('vector');
-                    // expect(status.source.type).to.be.equal('raster:vectorized');
-                    done();
-                });
-            })
-        });
+        //             expect(status.user_id).to.exist;
+        //             expect(status.file_id).to.exist;
+        //             expect(status.filename).to.be.equal(tmp.upload_status.filename);
+        //             expect(status.status).to.be.equal('Processing');
+        //             expect(status.data_type).to.be.equal('vector');
+        //             // expect(status.source.type).to.be.equal('raster:vectorized');
+        //             done();
+        //         });
+        //     })
+        // });
 
 
 
-        it('should be vectorized without errors', function (done) {
-            this.timeout(121000);
+        // it('should be vectorized without errors', function (done) {
+        //     this.timeout(121000);
             
-            // check for processing status
-            token(function (err, access_token) {
-                var processingInterval = setInterval(function () {
-                    process.stdout.write('.');
-                    api.get(endpoints.import.status)
-                    .query({ file_id : tmp.vectorized_status.file_id, access_token : access_token})
-                    .end(function (err, res) {
-                        if (err) return done(err);
+        //     // check for processing status
+        //     token(function (err, access_token) {
+        //         var processingInterval = setInterval(function () {
+        //             process.stdout.write('.');
+        //             api.get(endpoints.import.status)
+        //             .query({ file_id : tmp.vectorized_status.file_id, access_token : access_token})
+        //             .end(function (err, res) {
+        //                 if (err) return done(err);
 
-                        var status = helpers.parse(res.text);
+        //                 var status = helpers.parse(res.text);
 
-                        // wait for processing
-                        if (!status.processing_success) return;
+        //                 // wait for processing
+        //                 if (!status.processing_success) return;
 
-                        // manually assert
-                        if (!status.user_id) return done('user_id');
-                        if (!status.file_id) return done('file_id');
-                        if (status.filename != tmp.vectorized_status.filename) return done('filename');
-                        if (status.data_type != 'vector') return done('data_type');
-                        // if (status.source.type != 'raster:vectorized') return done('source.type');
+        //                 // manually assert
+        //                 if (!status.user_id) return done('user_id');
+        //                 if (!status.file_id) return done('file_id');
+        //                 if (status.filename != tmp.vectorized_status.filename) return done('filename');
+        //                 if (status.data_type != 'vector') return done('data_type');
+        //                 // if (status.source.type != 'raster:vectorized') return done('source.type');
 
-                        // all good!
-                        clearInterval(processingInterval);
-                        tmp.vectorized_status = status;
-                        done();
-                    });
-                }, 500);
-            });
-        });
+        //                 // all good!
+        //                 clearInterval(processingInterval);
+        //                 tmp.vectorized_status = status;
+        //                 done();
+        //             });
+        //         }, 500);
+        //     });
+        // });
 
-        it('should create a vector layer', function (done) {
-            this.timeout(40000);
-            token(function (err, access_token) {
+        // it('should create a vector layer', function (done) {
+        //     this.timeout(40000);
+        //     token(function (err, access_token) {
 
-                var layer = {
-                    geom_column: 'the_geom_3857',
-                    geom_type: 'geometry',
-                    raster_band: '',
-                    srid: '',
-                    affected_tables: '',
-                    interactivity: '',
-                    attributes: '',
-                    access_token: access_token,
-                    cartocss_version: '2.0.1',
-                    cartocss : '#layer { polygon-fill: yellow; polygon-opacity: 0.5; }',
-                    sql: '(SELECT * FROM ' + tmp.vectorized_status.file_id + ') as sub',
-                    file_id: tmp.vectorized_status.file_id,
-                    return_model: true,
-                }
+        //         var layer = {
+        //             geom_column: 'the_geom_3857',
+        //             geom_type: 'geometry',
+        //             raster_band: '',
+        //             srid: '',
+        //             affected_tables: '',
+        //             interactivity: '',
+        //             attributes: '',
+        //             access_token: access_token,
+        //             cartocss_version: '2.0.1',
+        //             cartocss : '#layer { polygon-fill: yellow; polygon-opacity: 0.5; }',
+        //             sql: '(SELECT * FROM ' + tmp.vectorized_status.file_id + ') as sub',
+        //             file_id: tmp.vectorized_status.file_id,
+        //             return_model: true,
+        //         }
                 
-                api.post(endpoints.tiles.create)
-                .send(layer)
-                .end(function (err, res) {
-                    if (err) return done(err);
+        //         api.post(endpoints.tiles.create)
+        //         .send(layer)
+        //         .end(function (err, res) {
+        //             if (err) return done(err);
 
-                    var status = res.body;
+        //             var status = res.body;
 
-                    if (debugMode) {
-                        console.log('\n\n\n');
-                        console.log('Layer returned from', endpoints.tiles.create, '[pile]:');
-                        console.log('------------------------------------------')
-                        console.log(status);
-                    }
+        //             if (debugMode) {
+        //                 console.log('\n\n\n');
+        //                 console.log('Layer returned from', endpoints.tiles.create, '[pile]:');
+        //                 console.log('------------------------------------------')
+        //                 console.log(status);
+        //             }
 
-                    expect(status.layerUuid).to.exist;
-                    expect(status.options.layer_id).to.exist;
-                    expect(status.options.file_id).to.be.equal(tmp.vectorized_status.file_id);
-                    expect(status.options.data_type).to.be.equal('vector');
+        //             expect(status.layerUuid).to.exist;
+        //             expect(status.options.layer_id).to.exist;
+        //             expect(status.options.file_id).to.be.equal(tmp.vectorized_status.file_id);
+        //             expect(status.options.data_type).to.be.equal('vector');
 
-                    tmp.vector_layer = status;
+        //             tmp.vector_layer = status;
 
-                    done();
-                });
-            });
-        });
+        //             done();
+        //         });
+        //     });
+        // });
 
 
-        it('should get expected raster-tile from vectorized raster', function (done) {
-            this.timeout(40000);
-            token(function (err, access_token) {
+        // it('should get expected raster-tile from vectorized raster', function (done) {
+        //     this.timeout(40000);
+        //     token(function (err, access_token) {
 
-                var type = 'png';
-                var tile = [7,67,37];
-                var layer_id = tmp.vector_layer.options.layer_id;
-                var tiles_url = base_tiles_url();
-                tiles_url += layer_id + '/' + tile[0] + '/' + tile[1] + '/' + tile[2] + '.' + type + '?access_token=' + access_token;
+        //         var type = 'png';
+        //         var tile = [7,67,37];
+        //         var layer_id = tmp.vector_layer.options.layer_id;
+        //         var tiles_url = base_tiles_url();
+        //         tiles_url += layer_id + '/' + tile[0] + '/' + tile[1] + '/' + tile[2] + '.' + type + '?access_token=' + access_token;
                 
-                // files (todo: cleanup)
-                var expected = 'test/open-data/vectorized-tile.expected.png';
-                var actual = 'test/tmp/vectorized-test-tile.png'
+        //         // files (todo: cleanup)
+        //         var expected = 'test/open-data/vectorized-tile.expected.png';
+        //         var actual = 'test/tmp/vectorized-test-tile.png'
 
-                http.get({
-                    url : tiles_url,
-                    // We don't need ssl validation during tests
-                    noSslVerifier : true
-                }, actual, function (err, result) {
-                    if (err) return done(err);
+        //         http.get({
+        //             url : tiles_url,
+        //             // We don't need ssl validation during tests
+        //             noSslVerifier : true
+        //         }, actual, function (err, result) {
+        //             if (err) return done(err);
 
-                    var e = fs.readFileSync(actual);
-                    var a = fs.readFileSync(expected);
-                    assert.ok(Math.abs(e.length - a.length) < 100);
-                    done();
-                });
-            });
-        }); 
+        //             var e = fs.readFileSync(actual);
+        //             var a = fs.readFileSync(expected);
+        //             assert.ok(Math.abs(e.length - a.length) < 100);
+        //             done();
+        //         });
+        //     });
+        // }); 
 
-        it.skip('should get expected vector-tile from vectorized raster', function (done) {
-            this.timeout(40000);
-            token(function (err, access_token) {
-                if (err) return done(err);
-                var type = 'pbf';
-                var tile = [10,570,234];
-                var tiles_url = base_tiles_url();
-                var layer_id = tmp.vector_layer.options.layer_id;
-                var tiles_url = subdomain.replace('{s}', config.servers.tiles.subdomains[0]);
-                tiles_url += layer_id + '/' + tile[0] + '/' + tile[1] + '/' + tile[2] + '.' + type + '?access_token=' + access_token;
+        // it.skip('should get expected vector-tile from vectorized raster', function (done) {
+        //     this.timeout(40000);
+        //     token(function (err, access_token) {
+        //         if (err) return done(err);
+        //         var type = 'pbf';
+        //         var tile = [10,570,234];
+        //         var tiles_url = base_tiles_url();
+        //         var layer_id = tmp.vector_layer.options.layer_id;
+        //         var tiles_url = subdomain.replace('{s}', config.servers.tiles.subdomains[0]);
+        //         tiles_url += layer_id + '/' + tile[0] + '/' + tile[1] + '/' + tile[2] + '.' + type + '?access_token=' + access_token;
                 
-                // files (todo: cleanup)
-                var expected = 'test/open-data/vectorized-tile.expected.pbf';
-                var actual = 'test/tmp/vectorized-test-tile.pbf'
+        //         // files (todo: cleanup)
+        //         var expected = 'test/open-data/vectorized-tile.expected.pbf';
+        //         var actual = 'test/tmp/vectorized-test-tile.pbf'
 
-                http.get({
-                    url : tiles_url,
-                    // We don't need ssl validation during tests
-                    noSslVerifier : true
-                }, actual, function (err, result) {
-                    if (err) return done(err);
+        //         http.get({
+        //             url : tiles_url,
+        //             // We don't need ssl validation during tests
+        //             noSslVerifier : true
+        //         }, actual, function (err, result) {
+        //             if (err) return done(err);
 
-                    var e = fs.readFileSync(actual);
-                    var a = fs.readFileSync(expected);
-                    assert.ok(Math.abs(e.length - a.length) < 100);
-                    done();
-                });
-            });
-        }); 
+        //             var e = fs.readFileSync(actual);
+        //             var a = fs.readFileSync(expected);
+        //             assert.ok(Math.abs(e.length - a.length) < 100);
+        //             done();
+        //         });
+        //     });
+        // }); 
 
     });
 
