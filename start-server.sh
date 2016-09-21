0d30f9f39e661739b0dfc3c46165306c3264466a
knutole <knutolesjoli@gmail.com>
2016-09-21 23:03:16 +0200

    Increased memory for Node to 8GB - using flag --max-old-space-size=8192 - see eg. http://www.codexpedia.com/javascript/increasing-the-memory-limit-in-node-js/

--- start-server.sh
+++ start-server.sh
@@ -13,6 +13,6 @@ if $SYSTEMAPIC_PRODMODE; then
 	echo 'Pile | PostGIS Tile Server | Production mode'
 	forever src/pile.js production >> log/pile.log
 else
-	echo 'Pile Debug mode'
-	nodemon -i node_modules/ -i tests/ src/pile.js
+	echo 'Pile Debug mode (with 8GB memory)'
+	nodemon --max-old-space-size=8192 -i node_modules/ -i tests/ src/pile.js
 fi
